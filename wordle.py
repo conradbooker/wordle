49c1bfc08118d0d823315f36238f9e2724ba9641
Conrad Booker <72096610+conradbooker@users.noreply.github.com>
2023-01-03 09:41:35 -0500

    Add files via upload

---
+++ wordle.py
@@ -0,0 +1,25 @@
+import colorama
+import itertools
+
+
+
+def getFeedback(guess, secretWord):
+    '''Generates a feedback string based on comparing guess with the secret word. 
+       The feedback string uses the following schema: 
+        - Correct letter, correct spot: uppercase letter
+        - Correct letter, wrong spot: lowercase letter
+        - Letter not in the word: '-'
+
+       For example:
+        - getFeedback("lever", "EATEN") --> "-e-E-"
+        - getFeedback("LEVER", "LOWER") --> "L--ER"
+        - getFeedback("MOMMY", "MADAM") --> "M-m--"
+        - getFeedback("ARGUE", "MOTTO") --> "-----"
+
+        Args:
+            guess (str): The guessed word
+            secretWord (str): The secret word
+        Returns:
+            str: Feedback string, based on comparing guess with the secret word
+    '''
+
